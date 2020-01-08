cdd90ffaa3e195ceeb5db987a1cd0ebb6e0dac60
wyj <aaa314568649@qq.com>
2020-01-08 18:44:06 +0800

    118:44

--- leyou-upload/src/test/java/com/leyou/upload/test/FastDFSTest.java
+++ leyou-upload/src/test/java/com/leyou/upload/test/FastDFSTest.java
@@ -1,4 +1,54 @@
 package com.leyou.upload.test;
 
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.domain.fdfs.ThumbImageConfig;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import com.github.tobato.fastdfs.service.TrackerClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class FastDFSTest {
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    @Autowired
+    private ThumbImageConfig thumbImageConfig;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        //找一张本地图片路径
+        File file = new File("C:\\Users\\wyj\\Desktop\\aaa.jpg");
+        // 上传并保存图片，参数：1-上传的文件流 2-文件的大小 3-文件的后缀 4-可以不管他
+        StorePath storePath = this.storageClient.uploadFile(
+                new FileInputStream(file), file.length(), "jpg", null);
+        // 带分组的路径
+        System.out.println(storePath.getFullPath());
+        // 不带分组的路径
+        System.out.println(storePath.getPath());
+    }
+
+    @Test
+    public void testUploadAndCreateThumb() throws FileNotFoundException {
+        //找一张本地图片路径
+        File file = new File("C:\\Users\\wyj\\Desktop\\aaa.jpg");
+        // 上传并且生成缩略图
+        StorePath storePath = this.storageClient.uploadImageAndCrtThumbImage(
+                new FileInputStream(file), file.length(), "jpeg", null);
+        // 带分组的路径
+        System.out.println(storePath.getFullPath());
+        // 不带分组的路径
+        System.out.println(storePath.getPath());
+        // 获取缩略图路径
+        String path = thumbImageConfig.getThumbImagePath(storePath.getPath());
+        System.out.println(path);
+    }
 }
