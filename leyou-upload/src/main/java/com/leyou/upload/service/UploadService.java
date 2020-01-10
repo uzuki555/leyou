11ab8992ed6420a9ee7b457f1c77899012bbab99
wyj <aaa314568649@qq.com>
2020-01-10 18:37:02 +0800

    1837

--- leyou-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ leyou-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -1,7 +1,11 @@
 package com.leyou.upload.service;
 
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,7 +22,8 @@ public class UploadService {
     private static final List<String> CONTENT_TYPES = Arrays.asList("image/gif","image/jpeg");
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadService.class);
-
+    @Autowired
+    private FastFileStorageClient storageClient;
     public String uploadImage(MultipartFile file) {
 
             String originalFilename = file.getOriginalFilename();
@@ -34,10 +39,13 @@ public class UploadService {
                 LOGGER.info("文件内容不合法：{}", originalFilename);
                 return null;
             }
-            file.transferTo(new File("E:\\leyouImage\\" + originalFilename));
-
-
-            return "http://image.leyou.com/" + originalFilename;
+//            file.transferTo(new File("E:\\leyouImage\\" + originalFilename));
+//
+//
+//            return "http://image.leyou.com/" + originalFilename;
+            String ext = StringUtils.substringAfterLast(originalFilename, ".");
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), ext, null);
+            return "http://image.leyou.com/"+storePath.getFullPath();
         }catch (IOException e){
             LOGGER.info("服务器内部错误：" + originalFilename);
             e.printStackTrace();
