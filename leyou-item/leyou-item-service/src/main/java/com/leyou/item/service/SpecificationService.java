1d06cd64328349c62022f18b4f48d8bb9e4bc1ac
wyj <aaa314568649@qq.com>
2020-01-11 18:34:35 +0800

    11 1834

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -0,0 +1,39 @@
+package com.leyou.item.service;
+
+
+import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.mapper.SpecParamMapper;
+import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class SpecificationService {
+
+    @Autowired
+    private SpecGroupMapper groupMapper;
+    @Autowired
+    private SpecParamMapper paramMapper;
+
+    public List<SpecGroup> queryGroupsByCid(Long cid) {
+//        Example example = new Example(SpecGroup.class);
+//        Example.Criteria criteria = example.createCriteria();
+//        criteria.andEqualTo("cid",cid);
+//        List<SpecGroup> groups = groupMapper.selectByExample(example);
+        SpecGroup record = new SpecGroup();
+        record.setCid(cid);
+        List<SpecGroup> groups = groupMapper.select(record);
+        return groups;
+    }
+
+    public List<SpecParam> queryParams(Long gid) {
+        SpecParam record = new SpecParam();
+        record.setGroupId(gid);
+        List<SpecParam> params = paramMapper.select(record);
+        return params;
+    }
+}
