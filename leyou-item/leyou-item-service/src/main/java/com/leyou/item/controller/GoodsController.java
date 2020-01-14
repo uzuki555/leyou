e5fc6ffb2c1ffabc0b044d01da1421d28f0f9b06
wyj <aaa314568649@qq.com>
2020-01-14 18:28:28 +0800

    14 1828

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/GoodsController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/GoodsController.java
@@ -2,22 +2,24 @@ package com.leyou.item.controller;
 
 import com.leyou.common.pojo.PageResult;
 import com.leyou.item.bo.SpuBo;
+import com.leyou.item.pojo.Sku;
+import com.leyou.item.pojo.SpuDetail;
 import com.leyou.item.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("spu")
 public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
-    @GetMapping("page")
+    @GetMapping("spu/page")
     public ResponseEntity<PageResult<SpuBo>> querySpuByPage(
             @RequestParam(value = "key",required = false)String key,
             @RequestParam(value = "seleable",required = false)Boolean seleable,
@@ -29,5 +31,31 @@ public class GoodsController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(result);
+    }
+    @PostMapping("goods")
+    public  ResponseEntity<Void> saveGoods(@RequestBody SpuBo spuBo){
+        this.goodsService.saveGoods(spuBo);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @GetMapping("spu/detail/{spuId}")
+    public  ResponseEntity<SpuDetail> querySpuDetailBySpuId(@PathVariable("spuId")Long spuId){
+        SpuDetail spuDetail = this.goodsService.querySpuDetailBySpuId(spuId);
+        if(spuDetail==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(spuDetail);
+    }
+    @GetMapping("sku/list")
+    public  ResponseEntity<List<Sku>> querySkusBySpuId(@RequestParam("id")Long spuId){
+        List<Sku> skus = this.goodsService.querySkusBySpuId(spuId);
+        if(CollectionUtils.isEmpty(skus)){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(skus);
+    }
+    @PutMapping("goods")
+    public  ResponseEntity<Void> updateGoods(@RequestBody SpuBo spuBo){
+        this.goodsService.updateGoods(spuBo);
+        return ResponseEntity.noContent().build();
     }
 }
--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/GoodsService.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/GoodsService.java
@@ -5,20 +5,19 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.leyou.common.pojo.PageResult;
 import com.leyou.item.bo.SpuBo;
-import com.leyou.item.mapper.BrandMapper;
-import com.leyou.item.mapper.SpuDetailMapper;
-import com.leyou.item.mapper.SpuMapper;
-import com.leyou.item.pojo.Brand;
-import com.leyou.item.pojo.Spu;
+import com.leyou.item.mapper.*;
+import com.leyou.item.pojo.*;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 import tk.mybatis.mapper.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +27,10 @@ public class GoodsService {
     private SpuMapper spuMapper;
     @Autowired
     private BrandMapper brandMapper;
+    @Autowired
+    private StockMapper stockMapper;
+    @Autowired
+    private SkuMapper skuMapper;
 
     @Autowired
     private SpuDetailMapper spuDetailMapper;
@@ -60,5 +63,68 @@ public class GoodsService {
         }).collect(Collectors.toList());
 
         return new PageResult<>(pageInfo.getTotal(),spuBos);
+    }
+    @Transactional
+    public void saveGoods(SpuBo spuBo) {
+        spuBo.setId(null);
+        spuBo.setSaleable(true);
+        spuBo.setValid(true);
+        spuBo.setCreateTime(new Date());
+        spuBo.setLastUpdateTime(spuBo.getCreateTime());
+        this.spuMapper.insertSelective(spuBo);
+        spuBo.getSpuDetail().setSpuId(spuBo.getId());
+        this.spuDetailMapper.insertSelective(spuBo.getSpuDetail());
+//        for(Sku sku : spuBo.getSkus()) {
+//            this.skuMapper.insertSelective(sku);
+//        }
+        saveSkuAndStock(spuBo);
+    }
+
+    private void saveSkuAndStock(SpuBo spuBo) {
+        spuBo.getSkus().forEach(sku -> {
+            sku.setId(null);
+            sku.setSpuId(spuBo.getId());
+            sku.setCreateTime(new Date());
+            sku.setLastUpdateTime(sku.getCreateTime());
+
+            this.skuMapper.insertSelective(sku);
+            Stock stock = new Stock();
+            stock.setSkuId(sku.getId());
+            stock.setStock(sku.getStock());
+            this.stockMapper.insertSelective(stock);
+        });
+    }
+
+    public SpuDetail querySpuDetailBySpuId(Long spuId) {
+        return  this.spuDetailMapper.selectByPrimaryKey(spuId);
+    }
+
+    public List<Sku> querySkusBySpuId(Long spuId) {
+        Sku record = new Sku();
+        record.setSpuId(spuId);
+        List<Sku> skus = this.skuMapper.select(record);
+        skus.forEach(sku -> {
+
+            sku.setStock(this.stockMapper.selectByPrimaryKey(sku.getId()).getStock());
+        });
+        return  skus;
+    }
+    @Transactional
+    public void updateGoods(SpuBo spuBo) {
+        Sku record = new Sku();
+        record.setSpuId(spuBo.getId());
+        List<Sku> skus = this.skuMapper.select(record);
+        skus.forEach(sku -> {
+            this.stockMapper.deleteByPrimaryKey(sku.getId());
+        });
+        Sku sku = new Sku();
+        sku.setSpuId(spuBo.getId());
+        this.skuMapper.delete(sku);
+        this.saveSkuAndStock(spuBo);
+        spuBo.setCreateTime(null);
+        spuBo.setLastUpdateTime(new Date());
+        spuBo.setSaleable(null);
+        this.spuMapper.updateByPrimaryKeySelective(spuBo);
+        this.spuDetailMapper.updateByPrimaryKeySelective(spuBo.getSpuDetail());
     }
 }
