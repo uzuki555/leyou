ed3a63ce6eec4e5b78e56657712a144b2b791c15
tom_glb <goodMorning_glb@atguigu.com>
2020-02-28 00:35:42 +0800

    035

--- leyou-cart/src/main/java/com/leyou/cart/controller/CartController.java
+++ leyou-cart/src/main/java/com/leyou/cart/controller/CartController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class CartController {
@@ -21,5 +26,14 @@ public class CartController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+    @GetMapping("/cart/list")
+    private ResponseEntity<List<Cart>> queryCarts(){
+        List<Cart> carts = this.cartService.queryCarts();
+        if(CollectionUtils.isEmpty(carts)){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(carts);
     }
 }
--- leyou-cart/src/main/java/com/leyou/cart/service/CartService.java
+++ leyou-cart/src/main/java/com/leyou/cart/service/CartService.java
@@ -10,17 +10,24 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CartService {
     @Autowired
     private StringRedisTemplate redisTemplate;
     @Autowired
     private GoodsClient goodsClient;
-
+    public  final String  PREFIX = "USER:CART:";
     public void addCart(Cart cart){
 
         UserInfo userInfo = CartUserInterceptor.getLoginUser();
-        BoundHashOperations<String, Object, Object> boundHashOps = redisTemplate.boundHashOps("USER:CART:" + userInfo.getId());
+
+        BoundHashOperations<String, Object, Object> boundHashOps = redisTemplate.boundHashOps(PREFIX + userInfo.getId());
         Integer num = cart.getNum();
 
         if(boundHashOps.hasKey(cart.getSkuId())){
@@ -40,5 +47,28 @@ public class CartService {
         }
         boundHashOps.put(userInfo.getId().toString(),JsonUtils.serialize(cart));
 
+    }
+
+    public List<Cart> queryCarts() {
+//        BoundHashOperations<String, Object, Object> boundHashOps = redisTemplate.boundHashOps(PREFIX + CartUserInterceptor.getLoginUser().getId());
+//        Map<Object, Object> map = boundHashOps.entries();
+//        List<Cart> list = new ArrayList<>();
+//        for(Map.Entry<Object,Object> cart : map.entrySet()){
+//            list.add(JsonUtils.parse((String)cart.getValue(),Cart.class));
+//        }
+//        return list;
+
+        String key = PREFIX + CartUserInterceptor.getLoginUser().getId();
+        if(!this.redisTemplate.hasKey(key)){
+            return null;
+        }
+        BoundHashOperations<String, Object, Object> boundHashOps = redisTemplate.boundHashOps(key);
+        List<Object> cartsJson = boundHashOps.values();
+        if(CollectionUtils.isEmpty(cartsJson)){
+            return null;
+        }
+        return  cartsJson.stream().map(cart ->{
+            return JsonUtils.parse(cart.toString(),Cart.class);
+        }).collect(Collectors.toList());
     }
 }
